af8df362a826908093a8e39f903b8136282ef12b
a487czz <leochp@gmail.com>
2017-07-14 16:41:58 -0300

    5. Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,7 @@ import { HeroService } from './hero.service';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li>
-        <li *ngFor="let hero of heroes"
+      <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
