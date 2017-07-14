3edcc24ed845c1c6ae4bb61d3efafbae695c1b9b
a487czz <leochp@gmail.com>
2017-07-14 15:51:23 -0300

    first

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Nice', power: 'Nice look' },
+    { id: 12, name: 'Narco', power: 'Cocaine?' },
+    { id: 13, name: 'Bombasto', power: 'Bomb' },
+    { id: 14, name: 'Celeritas', power: 'Celery' },
+    { id: 15, name: 'Magneta', power: 'Pink Magnet' },
+    { id: 16, name: 'RubberMan', power: 'Stealer' },
+    { id: 17, name: 'Dynama', power: 'Dynamic' },
+    { id: 18, name: 'Dr IQ', power: 'He is smart' },
+    { id: 19, name: 'Magma', power: 'magma' },
+    { id: 20, name: 'Tornado', power: 'tornado' },
+    { id: 21, name: 'Star Lord', power: 'Its rock, baby' },
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', power: 'Nice look' },
+  { id: 12, name: 'Narco', power: 'Cocaine?' },
+  { id: 13, name: 'Bombasto', power: 'Bomb' },
+  { id: 14, name: 'Celeritas', power: 'Celery' },
+  { id: 15, name: 'Magneta', power: 'Pink Magnet' },
+  { id: 16, name: 'RubberMan', power: 'Stealer' },
+  { id: 17, name: 'Dynama', power: 'Dynamic' },
+  { id: 18, name: 'Dr IQ', power: 'He is smart' },
+  { id: 19, name: 'Magma', power: 'magma' },
+  { id: 20, name: 'Tornado', power: 'tornado' },
+  { id: 21, name: 'Star Lord', power: 'Its rock, baby' },
+];
